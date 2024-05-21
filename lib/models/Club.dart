00fab57962bd6f1fb8829948242a58d9ea2fb417
AGersonNEU <98633255+AGersonNEU@users.noreply.github.com>
2024-05-21 00:55:18 -0600

    What is happening?

--- lib/models/Club.dart
+++ lib/models/Club.dart
@@ -1,27 +1,48 @@
-class MyData {
-  final String name;
-  final String description;
-  final bool isInactive;
+import 'package:json_annotation/json_annotation.dart';
 
-  MyData({
-    required this.name,
-    required this.description,
-    required this.isInactive,
+@JsonSerializable()
+class Club {
+  @JsonKey(name: "club_inactive")
+  String clubInactive;
+  @JsonKey(name: "club_name")
+  String clubName;
+  @JsonKey(name: "club_image")
+  String clubImage;
+  @JsonKey(name: "club_description")
+  String clubDescription;
+  @JsonKey(name: "club_id")
+  String clubId;
+  @JsonKey(name: "club_approved")
+  String clubApproved;
+
+  Club({
+    required this.clubInactive,
+    required this.clubName,
+    required this.clubImage,
+    required this.clubDescription,
+    required this.clubId,
+    required this.clubApproved,
   });
 
-  factory MyData.fromJson(Map<String, dynamic> json) {
-    return MyData(
-      name: json['name'],
-      description: json['description'],
-      isInactive: json['isInactive'],
+  factory Club.fromJson(Map<String, dynamic> json) {
+    return Club(
+      clubInactive: json["club_inactive"],
+      clubName: json["club_name"],
+      clubImage: json["club_image"],
+      clubDescription: json["club_description"],
+      clubId: json["club_id"],
+      clubApproved: json["club_approved"],
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
-      'name': name,
-      'description': description,
-      'isInactive': isInactive,
+      "club_inactive": this.clubInactive,
+      "club_name": this.clubName,
+      "club_image": this.clubImage,
+      "club_description": this.clubDescription,
+      "club_id": this.clubId,
+      "club_approved": this.clubApproved,
     };
   }
 }
