ee24284d709f212116144af098f0660afe549592
Michael Rowland <honeybadger448@gmail.com>
2024-05-28 13:12:51 -0600

    final push

--- lib/Pages/CreateNews.dart
+++ lib/Pages/CreateNews.dart
@@ -40,84 +40,86 @@ class _CreateNewsPage extends State<CreateNewsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: neuBar('Create News'),
-      body: SingleChildScrollView(
-        child: Align(
-          alignment: Alignment.center,
-          child: Column(
-            children: [
-              Padding(
+        appBar: neuBar('Create News'),
+        body: SingleChildScrollView(
+          child: Align(
+            alignment: Alignment.center,
+            child: Column(
+              children: [
+                Padding(
+                    padding: const EdgeInsets.fromLTRB(24, 16, 24, 0),
+                    child: inputField("Title", Icons.title, nameController)),
+                Padding(
                   padding: const EdgeInsets.fromLTRB(24, 16, 24, 0),
-                  child: inputField("Title", Icons.title, nameController)),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(24, 16, 24, 0),
-                child:
-                inputField("Description", Icons.description, descController),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(24, 16, 24, 0),
-                child: inputField("Location", Icons.map, locationController),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(24, 16, 24, 0),
-                child: inputField("Time", Icons.watch_later, timeController),
-              ),
-              const SizedBox(height: 20.0),
-              Container(
+                  child: inputField(
+                      "Description", Icons.description, descController),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(24, 16, 24, 0),
+                  child: inputField("Location", Icons.map, locationController),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(24, 16, 24, 0),
+                  child: inputField("Time", Icons.watch_later, timeController),
+                ),
+                const SizedBox(height: 20.0),
+                Container(
                   width: 200,
                   child: (fileToUpload != null)
                       ? Card(
-                    elevation: 10,
-                    shadowColor: Colors.amber,
-                    child: Column(
-                      children: [
-                        ClipRRect(
-                          borderRadius:
-                          BorderRadius.circular(20), // Image border
-                          child: SizedBox.fromSize(
-                            size: const Size.fromRadius(80), // Image radius
-                            child: Image.file(fileToUpload),
+                          elevation: 10,
+                          shadowColor: Colors.amber,
+                          child: Column(
+                            children: [
+                              ClipRRect(
+                                borderRadius:
+                                    BorderRadius.circular(20), // Image border
+                                child: SizedBox.fromSize(
+                                  size:
+                                      const Size.fromRadius(80), // Image radius
+                                  child: Image.file(fileToUpload),
+                                ),
+                              ),
+                              ElevatedButton.icon(
+                                  onPressed: _getImage,
+                                  label: const Text('Change image'))
+                            ],
                           ),
-                        ),
-                        ElevatedButton.icon(
-                            onPressed: _getImage,
-                            label: const Text('Change image'))
-                      ],
-                    ),
-                  )
+                        )
                       : OutlinedButton.icon(
-                    onPressed: _getImage,
-                    label: const Text(
-                        'No image selected, please select from gallery'),
-                  )),
-              Row(
-                children: [
-                  Padding(
-                      padding: const EdgeInsets.fromLTRB(180, 16, 24, 0),
-                      child: button(context, "Create News", () async {
-                        _title = nameController.text;
-                        _description = descController.text;
-                        _time = timeController.text;
-                        _location = locationController.text;
-                        if (_title.isEmpty ||
-                            _description.isEmpty ||
-                            fileToUpload == null ||
-                            _time.isEmpty ||
-                            _location.isEmpty) {
-                          showSnackBar(context, "Please fill out all fields!");
-                        } else {
-                          await CreateNews().whenComplete(() {
-                            Navigator.pop(context);
-                          });
-                        }
-                      }, 0, 0, 0, 0)),
-                ],
-              ),
-            ],
+                          onPressed: _getImage,
+                          label: const Text(
+                              'No image selected, please select from gallery'),
+                        ),
+                ),
+                Row(
+                  children: [
+                    Padding(
+                        padding: const EdgeInsets.fromLTRB(180, 16, 24, 0),
+                        child: button(context, "Create News", () async {
+                          _title = nameController.text;
+                          _description = descController.text;
+                          _time = timeController.text;
+                          _location = locationController.text;
+                          if (_title.isEmpty ||
+                              _description.isEmpty ||
+                              fileToUpload == null ||
+                              _time.isEmpty ||
+                              _location.isEmpty) {
+                            showSnackBar(
+                                context, "Please fill out all fields!");
+                          } else {
+                            await CreateNews().whenComplete(() {
+                              Navigator.pop(context);
+                            });
+                          }
+                        }, 0, 0, 0, 0)),
+                  ],
+                ),
+              ],
+            ),
           ),
-        ),
-      )
-    );
+        ));
   }
 
   Future<String> _uploadImage() async {
